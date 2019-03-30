18d15a4d9067ff9cb1fdd119dcae086603886eeb
Ramkishorerao Gujjeti <codecolonist@gmail.com>
2019-03-30 11:24:53 -0500

    Initial commit

---
+++ src/main/java/com/codecolonist/MahanyaPhotography/MahanyaPhotographyApplication.java
@@ -0,0 +1,13 @@
+package com.codecolonist.MahanyaPhotography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahanyaPhotographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahanyaPhotographyApplication.class, args);
+	}
+
+}
